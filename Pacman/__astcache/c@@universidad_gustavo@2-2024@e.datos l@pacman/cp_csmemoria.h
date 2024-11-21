5597d86ef958e5114b706144ac443481263e588e
zGustavopro <gustavopro078@gmail.com>
2024-11-21 17:27:25 -0400

    Proyectos Estructura de Datos 1 ,Ing. Winnie peg

---
+++ Pacman/__astcache/c@@universidad_gustavo@2-2024@e.datos l@pacman/cp_csmemoria.h
@@ -0,0 +1,24 @@
+﻿//---------------------------------------------------------------------------
+
+#ifndef CP_CSMemoriaH
+#define CP_CSMemoriaH
+#include "CSMemoria.h"
+//---------------------------------------------------------------------------
+using namespace std;
+
+class Pila_CSMemoria{
+private:
+	int tope;
+	CSMemoria* m;
+
+public:
+	Pila_CSMemoria();
+	Pila_CSMemoria(CSMemoria*);
+	bool vacia();
+	void meter(int);
+	void sacar(int&);
+	int sacar();
+	int cima();
+	void mostrar(int,int,TCanvas*);
+};
+#endif
